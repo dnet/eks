39dd88a378f8b410a32b0e7a1f5373f80361bbd4
András Veres-Szentkirályi <vsza@vsza.hu>
2014-03-11 17:44:54 +0100

    armor: added support for encoding

--- src/pgp_armor.erl
+++ src/pgp_armor.erl
@@ -1,5 +1,5 @@
 -module(pgp_armor).
--export([decode/1]).
+-export([decode/1, encode/1]).
 
 -define(CRC24_INIT, 16#B704CE).
 -define(CRC24_POLY, 16#1864CFB).
@@ -7,6 +7,9 @@
 -define(PGP_PUBKEY_HEADER, <<"-----BEGIN PGP PUBLIC KEY BLOCK-----">>).
 -define(PGP_PUBKEY_FOOTER, <<"-----END PGP PUBLIC KEY BLOCK-----">>).
 -define(PGP_VERSION_PREFIX, "Version: ").
+
+-define(LINE_LENGTH, 72).
+-define(EKS_BANNER, "EKS pre-release").
 
 decode(KeyText) ->
 	{KeyBody64, CRC} = keylines(binary:split(KeyText, <<$\n>>, [global])),
@@ -24,6 +27,16 @@ keylines([<<?PGP_VERSION_PREFIX, _/binary>> | Rest], Acc, CRC) -> keylines(Rest,
 keylines([<<$=, CRC/binary>> | Rest], Acc, _) -> keylines(Rest, Acc, CRC);
 keylines([?PGP_PUBKEY_FOOTER | _], Acc, CRC) -> {Acc, CRC};
 keylines([Line | Rest], Acc, CRC) -> keylines(Rest, <<Acc/binary, Line/binary>>, CRC).
+
+encode(KeyData) ->
+	[?PGP_PUBKEY_HEADER, $\n, ?PGP_VERSION_PREFIX, ?EKS_BANNER, $\n,
+	 $\n, encode_lines(base64:encode(KeyData)), $\n,
+	 $=, crc24b64(KeyData), $\n, ?PGP_PUBKEY_FOOTER].
+
+encode_lines(Data) -> encode_lines(Data, []).
+encode_lines(<<Line:?LINE_LENGTH/binary, Rest/binary>>, Acc) ->
+	encode_lines(Rest, [$\n, Line | Acc]);
+encode_lines(ShortLine, Acc) -> lists:reverse(Acc, [ShortLine]).
 
 crc24b64(Body) -> base64:encode(<<(crc24(Body)):24/integer-big>>).
 
